bef9f6dcdb2ddf09a367eeec5a49e61ebebaaa62
José Antonio Peñarrocha <113710914+joseantonio1714@users.noreply.github.com>
2025-10-14 21:30:03 +0200

    Afegida validació del correu i neteja de camps si no és vàlid

--- registre/src/App.vue
+++ registre/src/App.vue
@@ -1,19 +1,18 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import RegisterForm from './components/RegisterForm.vue'
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="App logo" class="logo" src="./assets/logo.png" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <RegisterForm />
     </div>
   </header>
 
   <main>
-    <TheWelcome />
   </main>
 </template>
 
