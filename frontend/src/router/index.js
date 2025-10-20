47b330d3dd0ce181cb3034af542e043cb7e8e02f
Grillooo <adriagare@gmail.com>
2025-10-21 00:57:26 +0200

    FrontEND de tot fet. Carpeta node-modules ha tindré en compte, uncomitteable. Tailwind instal·lat. Tot funcional a l'espera de la part d'editar perfil falta poder posar les imatges al frontend i connectar-ho amb el backend [Pol o Marc]

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,12 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Register from '../views/Register.vue';
 import Login from '../views/Login.vue';
 import Perfil from '../views/Perfil.vue'; 
+import LandingPage from '../views/LandingPage.vue';
 
 const routes = [
   { path: '/register', component: Register },
   { path: '/login', component: Login },
   { path: '/perfil', component: Perfil },
-  { path: '/', redirect: '/login' }   // simple por ahora
+  { path: '/', component: LandingPage }
 ];
 
 
