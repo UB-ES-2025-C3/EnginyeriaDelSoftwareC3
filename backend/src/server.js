2d62317a1f6341f329a30a37830da0420f21949a
Grillooo <adriagare@gmail.com>
2025-11-19 11:51:05 +0100

    Added Footer Component to GameCard

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,6 +22,7 @@ app.use(helmet());
 const allowedOrigins = [
   'https://calm-forest-0e7ca3203.3.azurestaticapps.net', 
   'https://witty-bay-0f8f41603.3.azurestaticapps.net', 
+  'http://localhost:5173',
   env.corsOrigin                                    
 ];
 
