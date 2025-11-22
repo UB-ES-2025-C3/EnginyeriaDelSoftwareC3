f1a77dfa8a5a59b21d68e39c62e85f1508eb3cd9
itzGery <gerardfideu24@gmail.com>
2025-11-22 23:05:13 +0100

    Se añade e2e de cataleg y flujo completo

--- frontend/cypress/e2e/cataleg.cy.js
+++ frontend/cypress/e2e/cataleg.cy.js
@@ -59,76 +59,10 @@ describe('User full flow: register -> login -> update profile', () => {
     // Si tras login vas al Catàleg:
     // cy.url().should('include', '/Cataleg');
 
-    // --------- 3) PERFIL (GET + PUT interceptados) ---------
-    cy.intercept('GET', '**/profile', {
-      statusCode: 200,
-      body: {
-        name,
-        email,
-        bio: 'This is a test bio.',
-        links: {
-          steam: 'https://steam.com/test',
-          twitch: 'https://twitch.tv/test',
-        },
-        avatarUrl: null,
-        bannerUrl: null,
-      },
-    }).as('getProfile');
-
-    cy.intercept('PUT', '**/profile', {
-      statusCode: 200,
-      body: { success: true },
-    }).as('updateProfile');
-
-    cy.visit('/perfil');
-
-    // --------- 4) RELLENAR CAMPOS PERFIL ---------
-    const newBio    = 'Updated bio text';
-    const newSteam  = 'https://steam.com/updated';
-    const newTwitch = 'https://twitch.tv/updated';
-    const newPSN    = 'PSN-User-Updated';
-    const newXbox   = 'Xbox-User-Updated';
-
-    cy.contains('h2', 'El Meu Perfil').should('be.visible').wait(1000);
-
-    cy.get('#bio')
-      .should('be.visible')
-      .clear()
-      .type(newBio)
-      .should('have.value', newBio);
-
-    cy.get('#steam')
-      .clear()
-      .type(newSteam)
-      .should('have.value', newSteam);
-
-    cy.get('#twitch')
-      .clear()
-      .type(newTwitch)
-      .should('have.value', newTwitch);
-
-    cy.get('#psn')
-      .clear()
-      .type(newPSN)
-      .should('have.value', newPSN);
-
-    cy.get('#xbox')
-      .clear()
-      .type(newXbox)
-      .should('have.value', newXbox);
-
-    cy.contains('button', 'Desar canvis')
-      .should('not.be.disabled')
-      .click();
-
-    // Mensaje de éxito (ajusta el texto si es distinto)
-    cy.contains('Perfil desat correctament').should('be.visible');
-
     cy.visit('/cataleg').wait(2000);
 
     // Ens fiquem a la fitxa d'aquest joc per veure que tota la informació s'ha guardat correctament
     cy.contains('God of War Ragnarök').click({ force: true});
-    
     
   });
 });
