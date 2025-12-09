34dfcb3c9ef5c93d9803e85c1a4c5b92bdbe90ac
itzGery <gerardfideu24@gmail.com>
2025-12-09 11:00:26 +0100

    yyy

--- frontend/cypress/e2e/add-review.cy.js
+++ frontend/cypress/e2e/add-review.cy.js
@@ -29,7 +29,7 @@ describe('User flow: register -> catàleg -> game -> write review', () => {
     cy.contains('button', 'Registrar-me')
       .should('exist')
       .click()
-      .wait(3000);
+      .wait(2000);
 
     // ---------- 2) LOGIN (si tu app no deja logueado tras el registro) ----------
     cy.visit('/login');
